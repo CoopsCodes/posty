65040076d05196b975d80db8922140985d35ccd8
Cooper Viktor <im.cooperviktor@gmail.com>
2020-12-27 16:02:08 +1100

    hooking up tailwindcss

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app') {{-- this points to the directory, PHP uses dot notation rather than slash. this points to the layouts folder and the app file. --}}
+
+@section('content') {{-- this points to the '@yield('content')' location in the file described above --}}
+	Index
+@endsection {{-- I am an end --}}
